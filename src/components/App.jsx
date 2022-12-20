13fb6e6360fb5dd240e20acc27f6aca84ca83d67
Maksym Stelmakh <stelmakh1994@gmail.com>
2022-12-20 14:20:29 +0200

    add saving contacts to localeStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,22 @@ import styles from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts.length > 0) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
 
   normalizeValue = value => value.toLowerCase().trim();
 
